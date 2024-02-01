5d5b499a59b93f6363f81175636d44216989f3ad
harithashaji <harithashaji2023a@mca.ajce.in>
2024-02-02 01:04:25 +0530

    sale invoice

--- app/Tbl_sale_order_trans.php
+++ app/Tbl_sale_order_trans.php
@@ -5,13 +5,24 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Tbl_sale_order_trans extends Model
-{
+{ 
+    protected $table = 'tbl_sale_order_trans'; // Specify the correct table name
+    
+
     public function saleOrderMaster()
     {
-        return $this->belongsTo(Tbl_sale_order_masters::class, 'sale_order_id');
-        
+        return $this->belongsTo(Tbl_sale_order_masters::class, 'sale_order_id', 'id');
     }
 
-protected $fillable = ['order_id', 'product_id', 'sale_order_id', 'qty', 'offer_amount', 'price', 'taxable_amount'];
+    protected $fillable = [
+        'order_id',
+        'product_id',
+        'sale_order_id',
+        'qty',
+        'offer_amount',
+        'price',
+        'taxable_amount',
+        // Add other attributes as needed
+    ];
 
 }
