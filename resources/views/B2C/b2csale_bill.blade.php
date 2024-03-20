ffc46d78f856f42bbb910d36b05b14b75e56d068
harithashaji <harithashaji2023a@mca.ajce.in>
2024-03-20 14:13:31 +0530

    invoice

--- resources/views/B2C/b2csale_bill.blade.php
+++ resources/views/B2C/b2csale_bill.blade.php
@@ -320,7 +320,8 @@
         </div>
         <div class="content left-content">
             <p>{{$key->name}}<br>
-                
+            <p>{{ $key->area }}, {{ $key->area1 }}, {{ $key->city }}, {{ $key->district }}, {{ $key->state }}, {{ $key->country }}, {{ $key->pincode }}</p>
+
             </p>
         </div>
     </div>
