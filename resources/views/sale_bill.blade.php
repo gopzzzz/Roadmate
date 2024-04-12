19d469e0473962d8bba2421fd119effb59d2b1d3
Bhadra2023 <bhadrarajan2023a@mca.ajce.in>
2024-04-12 18:35:48 +0530

    commit

--- resources/views/sale_bill.blade.php
+++ resources/views/sale_bill.blade.php
@@ -310,214 +310,113 @@ E-Mail: info@roadmate.in</th><th  colspan="3" style="font-weight: normal;">Invoi
 {{ $key->area }} {{ $key->area1 }} {{ $key->city }}<br>{{ $key->district }}, {{ $key->state }}<br>Mob: {{ $key->phone }}<br>{{ $key->state }} - {{ $key->pincode }},  {{ $key->country }}<br>State Name: {{ $key->state }}
 </th><th colspan="6" style="font-weight: normal; text-align: left; vertical-align: top;">Terms of Delivery<br></th></tr>
 
-
 <thead>
-<tr>
-<th style="font-weight: normal;">Sl<br>No.</th>
-<th style="font-weight: normal;">Description of Goods</th>
-<th style="font-weight: normal;">HSN/SAC</th>
-<th style="font-weight: normal;">GST Rate</th>
-<th style="font-weight: normal;  width: 5%;">Quantity</th>
-<th style="font-weight: normal;">Rate</th>
-<th style="font-weight: normal; width: 5%;">Per</th>
-<th style="font-weight: normal;" colspan="2">Amount</th>
-</tr>
+    <tr>
+    <th style="width: 5%; font-weight: normal;">Sl No.</th>
+        <th style="font-weight: normal;">Description</th>
+        <th style="font-weight: normal;">Unit Price</th>
+        <th style="font-weight: normal;">Quantity</th>
+        <th style="font-weight: normal;  ">HSN Code</th>
+        <th style="font-weight: normal;">SGST</th>
+        <th style="font-weight: normal;">CGST</th>
+        <th style="font-weight: normal; ">Tax.Amt</th>
+
+        <th style="font-weight: normal;" colspan="4">Amount</th>
+    </tr>
 </thead>
 <tbody>
-            @php  
-$i=1;
-$sum=0;   
-$taxableamount=0;
-$subtotal=0;
-$totalQuantity = 0;
-    $totalSum = 0;
-    $totalcgst=0;
-    $totalsgst=0;
-    $totalsubtotal=0;
-    $totaltaxamount=0;
-@endphp
-            @foreach($salebill as $key)
-            @php 
-            $unitprice=(($key->offer_amount)/(1+(($key->tax)/100)));
-            $taxamount=($unitprice*($key->tax/100));
+@php  
+    $i = 1;
+    $sum = 0;
+    $taxableamount = 0;
+    $subtotal = 0;
+   
+@endphp  
 
-$unitprice = number_format($unitprice, 2, '.', '');
-$taxamount = number_format($taxamount, 2, '.', '');
-$totalQuantity += $key->qty;
-        $totalSum += $key->qty * $key->offer_amount;
-        $totalcgst=($totalSum * ($key->igst / 100));
-        $totalsgst=($totalSum * ($key->cgst / 100));
-        $totalsubtotal=($totalSum + $totalcgst + $totalsgst);
-        $totaltaxamount=($totalcgst + $totalsgst);
-            @endphp
-            <tr> 
-        <td>{{$i++}}</td>
-        <td><b>{{$key->product_name}}</b></td>
-        <td></td>
-        <td style="text-align: right;">{{$key->tax}} % </td>
-        <td style="text-align: right;"><b>{{$key->qty}} NOS</b></td>
-        <td style="text-align: right;">{{$key->offer_amount}}</td>
-        <td style="text-align: right;">NOS</td>
-        <td style="text-align: right;" colspan="2"><b>{{$key->qty * $key->offer_amount}}</b></td>
-    </tr>
-@endforeach
-
-<tr>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td style="border-top: 2px solid #000000; text-align: right;" colspan="2">{{$totalSum}}</td>
-</tr>
-</tbody>
-
-         
-                <tr>
-    <td style="font-weight: normal;">&nbsp;</td>
-    <td style="font-weight: normal; text-align: right;"><b>SGST</b></td>
-    <td style="font-weight: normal;">&nbsp;</td>
-    <td style="font-weight: normal;">&nbsp;</td>
-    <td style="font-weight: normal;">&nbsp;</td>
-    <td style="font-weight: normal; text-align: right;">{{$key->cgst}}</td>
-    <td style="font-weight: normal; text-align: left;"> %</td>
-    <td style="font-weight: normal; text-align: right;" colspan="2"><b>{{$totalsgst}}</b></td>
-</tr>
-<tr>
-    <td style="font-weight: normal;">&nbsp;</td>
-    <td style="font-weight: normal; text-align: right;"><b>CGST</b></td>
-    <td style="font-weight: normal;">&nbsp;</td>
-    <td style="font-weight: normal;">&nbsp;</td>
-    <td style="font-weight: normal;">&nbsp;</td>
-    <td style="font-weight: normal; text-align: right;">{{$key->igst}}</td>
-    <td style="font-weight: normal; text-align: left;"> %</td>
-    <td style="font-weight: normal; text-align: right;" colspan="2"><b>{{$totalcgst}}</b></td>
-</tr>
-<tr> 
-        <td style="text-align: right;"></td>
-        
-        <td style="text-align: right;"></td>
-        <td style="text-align: right;"></td>
-        <td style="text-align: right;"></td>
-        <td style="text-align: right;"></td>
-        <td style="text-align: right;"></td>
-        <td style="text-align: right;"></td>
-        <td style="text-align: right;" colspan="2"></td>
-    </tr>
+@foreach($salebill as $key)
+    @php 
+        $unitprice = (($key->offer_amount) / (1 + (($key->tax) / 100)));
+        $taxamount = ($unitprice * ($key->tax / 100));
+        $unitprice = number_format($unitprice, 2, '.', '');
+        $taxamount = number_format($taxamount, 2, '.', '');
+   
+    @endphp
     <tr> 
-        <td style="text-align: right;" ></td>
-        <td style="text-align: right;"></td>
-        <td style="text-align: right;"></td>
-        <td style="text-align: right;"></td>
-        <td style="text-align: right;"></td>
-        <td style="text-align: right;"></td>
-        <td style="text-align: right;"></td>
-        <td style="text-align: right;" colspan="2"></td>
+        <td>{{$i}}</td>
+        <td><b>{{$key->product_name}}</b></td>
+        <td>{{$unitprice}}</td>
+        <td style="text-align: right;"><b>{{$key->qty}}</b></td>
+        <td style="text-align: right;">{{$key->hsncode}}  </td>
+
+        <td style="text-align: right;">{{$key->cgst}} %</td>
+        <td style="text-align: right;">{{$key->igst}} %</td>
+        <td style="text-align: right;" ><b>{{$taxamount * $key->qty}}</b></td>
+        <td style="text-align: right;"><b> {{$key->qty*$key->offer_amount}}</b></td>
+
     </tr>
-                @php 
+</tbody>
+     @php 
                 $sum += $key->qty*$key->offer_amount;
 $taxableamount += $taxamount*$key->qty;
 $subtotal+= $key->qty*$unitprice;
 $i++;
-
+$totalsubtotal= ($sum + $key->shipping_charge) ;
                 @endphp
-              
-                <!-- Add more rows as needed -->
+                @endforeach
                 </tbody>
+           
+                <tr class="total-row">
+                <td></td>
+                    <td style="border: 2px solid #000000;">Total</td>
+                    <td style="border: 2px solid #000000";></td>
+                    <td style="border: 2px solid #000000";></td>
+                    <td style="border: 2px solid #000000";></td>
+
+                    <td style="border: 2px solid #000000";></td>
+                    <td style="border: 2px solid #000000";></td>
+                    <td></td>
+                    <td></td>
+                    
+                </tr>
+         
+         
           
            
-          <tr class="total-row">
-          <td style="border: 2px solid #000000;"></td>
-              <td style="border: 2px solid #000000; text-align: right;">Total</td>
-              <td style="border: 2px solid #000000";></td>
-              <td style="border: 2px solid #000000";></td>
-              <td style="border: 2px solid #000000; font-size: 15px; text-align: right;"><b>{{$totalQuantity}} NOS</b></td>
-
-              <td style="border: 2px solid #000000";></td>
-              <td style="border: 2px solid #000000";></td>
-              <td style="border: 2px solid #000000; font-size: 20px; text-align: right;" colspan="2"><b>₹ {{ $totalsubtotal}}</b> </td>
-              
-          </tr>
-          <tr>    <td style="border-left: 1px solid #000000; border-right: none; font-weight: normal; text-align: left;" colspan="5">Amount Chargeable (in words)</td>
-<td style="border-right:1px solid #000000; border-left: none; font-weight: normal; text-align: right;" colspan="4"><i>E. & O.E</i></td>
-</tr>
-
-</tr>
-<tr>    <td style="font-weight: normal; text-align: left; font-size: 16px; border-bottom: 1px solid #000000;"  colspan="9"><b>INR {{ numberToWords($totalsubtotal) }} Only</b></td> </tr>   
-   
-<tr>
-<!-- QR code image on the right side -->
-<th style="text-align: right;"  rowspan="6">
-  <img src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Total%20Amount%3A%20{{ $totalsubtotal }}" alt="QR Code">
-</th>
-<!-- Table on the left side -->
-
-      <!-- Table rows -->
-
-          <th style="font-weight: normal; text-align: right;" colspan="2" rowspan="2">Taxable<br> Value</th>
-          <th style="font-weight: normal;" colspan="2">CGST</th>
-          <th style="font-weight: normal;" colspan="2">SGST/UTGST</th>
-          <th style="font-weight: normal;" colspan="2" rowspan="2">Total Tax Amount</th>
-</tr>
-      <tr>
-        
        
-
-          <th style="font-weight: normal;">Rate</th>
-          <th style="font-weight: normal;">Amount</th>
-          <th style="font-weight: normal;">Rate</th>
-          <th style="font-weight: normal;">Amount</th>
-      </tr>
-      <tr> 
-
-      <td style="text-align: right;" colspan="2">{{$totalSum}}</td>
-          <td style="text-align: right;">{{$key->igst}} %</td>
-          <td style="text-align: right;">{{$totalcgst}}</td>
-          <td style="text-align: right;">{{$key->cgst}} %</td>
-          <td style="text-align: right;">{{$totalsgst}}</td>
-          <td style="text-align: right;">{{ $totaltaxamount}}</td>
-      </tr>
-      <!-- More table rows can be added here -->
- 
-</td>
-</tr>
-</tr>
-<tr> 
+  <tr  style="text-align: right;" colspan="4">
+    <td  style="text-align: right;  border-right: none;" colspan="5"><strong><span style="color: grey;"><b>SUBTOTAL:</b></span></strong></td>
+    <td  style="text-align: right; border-left: none;" colspan="4"><span class="highlight-back">₹{{ number_format($subtotal,2) }}</span></td>
+  </tr>
+  <tr  style="text-align: right;">
+    <td  style="text-align: right;  border-right: none;" colspan="5"><strong><span style="color: grey;"><b>DELIVERY CHARGE:</b></span></strong></td>
+    <td  style="text-align: right; border-left: none;" colspan="4"><span class="highlight-back">{{ $key->shipping_charge }}</span></td>
+  </tr>
+  <tr  style="text-align: right;" colspan="4">
+    <td  style="text-align: right;  border-right: none;" colspan="5"><strong><span style="color: grey;"><b>(TAX RATE):</b></span></strong></td>
+    <td  style="text-align: right; border-left: none;"  colspan="4"><span class="highlight-back">₹{{ number_format($taxableamount,2) }}</span></td>
+  </tr>
   
-  <td style="text-align: right;"  colspan="2"></td>
-  <td style="text-align: right;"></td>
-  <td style="text-align: right;"></td>
-  <td style="text-align: right;"></td>
-  <td style="text-align: right;"></td>
-  <td style="text-align: right;"></td>
-</tr>
-<tr> 
-  <td style="text-align: right;"  colspan="2"></td>
-  <td style="text-align: right;"></td>
-  <td style="text-align: right;"></td>
-  <td style="text-align: right;"></td>
-  <td style="text-align: right;"></td>
-  <td style="text-align: right;"></td>
+  <tr  style="text-align: right;" colspan="4">
+    <td  style="text-align: right; border-right: none;"  colspan="5" ><strong><span style="color: grey;"><b>TOTAL:</b></span></strong></td>
+    <td  style="text-align: right; border-left: none;" colspan="4"><span class="highlight-back">₹{{ $totalsubtotal }}</span></td>
+  </tr>
+
+  </tbody>
+
+                <tr>    <td style="border-left: 1px solid #000000; border-right: none; font-weight: normal; text-align: left;" colspan="5">Amount Chargeable (in words)</td>
+    <td style="border-right:1px solid #000000; border-left: none; font-weight: normal; text-align: right;" colspan="4"><i>E. & O.E</i></td>
 </tr>
 
-<tr class="total-row">
-<td style="border: 2px solid #000000; text-align: right; " colspan="2"><b>Total:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
-  {{$totalSum}}</b></td>
-              <td style="border: 2px solid #000000; text-align: right;"><b></b></td>
-              <td style="border: 2px solid #000000; text-align: right;"><b>{{$totalcgst}}</b></td>
-              <td style="border: 2px solid #000000; text-align: right;"><b></b></td>
-              <td style="border: 2px solid #000000; font-size: 15px; text-align: right;"><b>{{$totalsgst}}</b></td>
-
-              <td style="border: 2px solid #000000; text-align: right;"><b>{{ $totaltaxamount}}</b></td>
-             
-              
-          </tr>
-</div>
-
+    </tr>
+    <tr>    <td style="font-weight: normal; text-align: left; font-size: 16px; "  colspan="9"><b>INR {{ numberToWords($totalsubtotal) }} Only</b></td> </tr>   
+         
+    <tr>
+    <!-- QR code image on the right side -->
+    <th style="text-align:left; border-bottom: none; border-right: none;"  colspan="1">
+        <img src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Total%20Amount%3A%20{{ $totalsubtotal }}" alt="QR Code">
         
-          <tr>    <td style="font-weight: normal; text-align: left; font-size: 16px;"  colspan="9">Tax Amount(in words) : <b>INR {{ numberToWords($totaltaxamount) }} Only</b></td> </tr>   
+    </th>     <th  colspan="8" style="border-bottom: none; border-left: none;"></th>
+        
           <tr>    <td style="font-weight: normal; text-align: left;"  colspan="9"><u>Declaration</u></td> </tr>   
           <tr>    <td style="font-weight: normal; text-align: left; border-bottom: 1px solid #000000;"  colspan="9">We declare that this invoice shows the actual price of the goods described and that all particulars are true and correct.</td> </tr>   
 
